47a881ad9e54c2e75775bc23df7b6f6146c90ed9
David Dahl <david@ddahl.com>
2012-05-31 09:48:55 -0500

    created algorithm-discovery.js

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 web-crypto-ideas
 ================
 
-JS examples and IDL ideas to work out before adding to the Web Crypto spec
+JS examples and IDL ideas to work out before adding to the Web Crypto API spec here: 
+  http://www.w3.org/2012/webcrypto/WebCryptoAPI/
---
+++ algorithm-discovery.js
@@ -0,0 +1,26 @@
+// Discover what algorithms are supported by the different methods, using IETF JOSE JWA, see: http://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-02
+
+// ** Block Encryption **
+
+window.crypto.pk.algorithms.blockenc;
+// returns array of strings: ["A128CBC", "A256CBC", "A128GCM","A256GCM"]
+
+// ** Key Agreement / Encryption **
+
+window.crypto.pk.algorithms.keyenc;
+// returns array of strings: ["RSA1_5", "RSA-OAEP", "ECDH-ES", "A128KW", "A256KW"]
+
+// ** Signature algorithms **
+
+window.crypto.sign.algorithms;
+// returns array of strings: ["RS256", "RS384", "RS512", "ES256", "ES384", "ES512"]
+
+// ** HMAC **
+
+window.crypto.hmac.algorithms;
+// returns array of strings: ["HS256", "HS384", "HS512"]
+
+// ** Hash **
+
+window.crypto.hash.algorithms;
+// returns array of strings: ["MD5", "SHA1", "SHA2"]
